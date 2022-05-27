9dec107ec7b978a0fac3699b328a3d320ce2659b
Manuel01856 <l21211932@tectijuana.edu.mx>
2022-05-27 11:22:56 -0700

    version 1

---
+++ excepciones-cSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepciones_cSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresé un numero");
+            int num1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresé otro numero");
+            int num2= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1/num2);
+            Console.ReadLine();
+
+            try
+            {
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
